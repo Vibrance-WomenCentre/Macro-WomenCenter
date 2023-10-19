868d32979ace4d41f54cde25d9ff8119c99b7cf6
Syahrul Ghifari Almubaraq <syahrulghifari47@gmail.com>
2023-10-19 22:31:12 +0700

    Rull

--- app/src/main/java/com/example/womencenter/AfterBayar.kt
+++ app/src/main/java/com/example/womencenter/AfterBayar.kt
@@ -5,13 +5,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import com.example.womencenter.ui.home.HomeFragment
 
 class AfterBayar : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_after_bayar)
-
+        val btnback : Button = findViewById(R.id.btn_back)
+        btnback.setOnClickListener{
+            val bb = Intent(this@AfterBayar, MainActivity::class.java)
+            startActivity(bb)
+        }
         val btncallend: Button = findViewById(R.id.btn_calll)
+
         btncallend.setOnClickListener {
 
             val bb = Intent(this@AfterBayar, Call::class.java)
